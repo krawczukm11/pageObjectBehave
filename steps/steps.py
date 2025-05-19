ec8712b960a56678d1d1ddc9e5b5c4376b8ab317
krawczukm11 <61738857+krawczukm11@users.noreply.github.com>
2025-05-19 19:12:56 +0200

    zzz

--- steps/steps.py
+++ steps/steps.py
@@ -16,42 +16,27 @@ DB_NAME = 'neondb'
 
 def polacz():
     conn = None  # Inicjalizuj conn na None
-    try:
-        conn = psycopg2.connect(host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
-        cursor = conn.cursor()
-        cursor.execute("SELECT version();")
-        db_version = cursor.fetchone()
-        print(f"Połączono z PostgreSQL w Neon Tech, wersja: {db_version}")
-        cursor.close()
-        return conn  # Zwróć obiekt połączenia
-    except psycopg2.Error as e:
-        print(f"Błąd połączenia z bazą danych Neon Tech: {e}")
-        if conn:
-            conn.close()
-        return None
+    conn = psycopg2.connect(host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    db_version = cursor.fetchone()
+    print(f"Połączono z PostgreSQL w Neon Tech, wersja: {db_version}")
+    cursor.close()
+    return conn  # Zwróć obiekt połączenia
+
 
 def stworz_tabele(conn):
-    if conn:
-        cursor = conn.cursor()
-        try:
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS oferty (
-                    id SERIAL PRIMARY KEY,
-                    tytul TEXT,
-                    link TEXT UNIQUE,
-                    zdjecie TEXT,
-                    cena TEXT,
-                    telefon TEXT
-                )
-            ''')
-            conn.commit()
-            print("Tabela 'oferty' została utworzona (lub już istnieje).")
-        except psycopg2.Error as e:
-            print(f"Błąd podczas tworzenia tabeli 'oferty': {e}")
-            conn.rollback()  # Wycofaj transakcję w przypadku błędu
-        cursor.close()
-    else:
-        print("Nie można utworzyć tabeli, brak połączenia z bazą danych.")
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS oferty (
+            id SERIAL PRIMARY KEY,
+            tytul TEXT,
+            link TEXT UNIQUE,
+            cena TEXT,
+            telefon TEXT
+        )
+    ''')
+    conn.commit()
 
 if __name__ == "__main__":
     conn = polacz()
